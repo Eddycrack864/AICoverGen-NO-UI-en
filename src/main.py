0cd42f04f007ffc31a0a18c00f3ed84ee69eb3a7
blane187gt <167639579+blane187gt@users.noreply.github.com>
2024-08-31 11:38:04 +0700

    Update for showing audio after inference

--- src/main.py
+++ src/main.py
@@ -353,3 +353,9 @@ if __name__ == '__main__':
                                      reverb_dry=args.reverb_dryness, reverb_damping=args.reverb_damping,
                                      output_format=args.output_format)
     print(f'[+] Cover generated at {cover_path}')
+
+
+print(f"showing {cover_path}")
+
+AudioSegment.from_file(cover_path)
+
