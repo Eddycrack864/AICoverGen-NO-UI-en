102aa32add6671c45beadf1e5e439d8863545f7d
blane187gt <167639579+blane187gt@users.noreply.github.com>
2024-08-31 13:08:56 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -313,7 +313,4 @@ if __name__ == '__main__':
                                reverb_rm_size=args.reverb_size, reverb_wet=args.reverb_wet, reverb_dry=args.reverb_dry, reverb_damping=args.reverb_damp, output_format=args.output_format)
 
 
-print(f"showing {ai_cover_path}")
-
-AudioSegment.from_file(ai_cover_path)
 
