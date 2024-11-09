923d488755a59afb2474b4d48a078917a3b61098
Ryouko <153080407+Ryouko-Yamanda65777@users.noreply.github.com>
2024-11-10 02:14:34 +0700

    forgot one line

--- src/infer_pack/models.py
+++ src/infer_pack/models.py
@@ -11,7 +11,7 @@ from torch.nn import Conv1d, ConvTranspose1d, AvgPool1d, Conv2d
 from torch.nn.utils import weight_norm, remove_weight_norm, spectral_norm
 from src.infer_pack.commons import init_weights
 import numpy as np
-from infer_pack import commons
+from src.infer_pack import commons
 
 
 class TextEncoder256(nn.Module):
