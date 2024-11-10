af6feae3b26728bbf5910247b25e89946475d24d
Eddycrack 864 <89285504+Eddycrack864@users.noreply.github.com>
2024-11-10 22:12:58 +0000

    Revert "Merge pull request #3 from Ryouko-Yamanda65777/main"
    This reverts commit d9ee96b5c6dc4bf0b98b1500c14bfbc3a8419ab6, reversing
    changes made to 71b8ab6508e27c57b7336c5c605b1004d1c0a76f.

--- src/rvc.py
+++ src/rvc.py
@@ -5,14 +5,14 @@ import torch
 from fairseq import checkpoint_utils
 from scipy.io import wavfile
 
-from src.infer_pack.models import (
+from infer_pack.models import (
     SynthesizerTrnMs256NSFsid,
     SynthesizerTrnMs256NSFsid_nono,
     SynthesizerTrnMs768NSFsid,
     SynthesizerTrnMs768NSFsid_nono,
 )
-from src.my_utils import load_audio
-from src.vc_infer_pipeline import VC
+from my_utils import load_audio
+from vc_infer_pipeline import VC
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
